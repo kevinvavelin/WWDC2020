a0b22b963b0921398bb51f8ba7c656fa22dcca43
Kevin Vavelin <kvavelin@geek-inc.fr>
2020-06-23 21:23:33 +0200

    Begin Ephemhelp. The start project with SwiftUI. Trying to make something of it

---
+++ Ephemhelp/Ephemhelp/EphemhelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  EphemhelpApp.swift
+//  Ephemhelp
+//
+//  Created by Kevin Vavelin on 23/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct EphemhelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
