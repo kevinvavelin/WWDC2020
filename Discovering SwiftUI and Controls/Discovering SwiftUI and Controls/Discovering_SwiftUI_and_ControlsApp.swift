7fe063a1e93111783ecdbe48c4b1043f05fdbfbc
Kevin Vavelin <kvavelin@geek-inc.fr>
2020-06-23 11:43:35 +0200

    Initial Commit

---
+++ Discovering SwiftUI and Controls/Discovering SwiftUI and Controls/Discovering_SwiftUI_and_ControlsApp.swift
@@ -0,0 +1,23 @@
+//
+//  Discovering_SwiftUI_and_ControlsApp.swift
+//  Discovering SwiftUI and Controls
+//
+//  Created by Kevin Vavelin on 23/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct Discovering_SwiftUI_and_ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct Discovering_SwiftUI_and_ControlsApp_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
