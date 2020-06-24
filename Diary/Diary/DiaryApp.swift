b7f4872f0f39e3553bc58c442be9c84b6e5b5d86
Kevin Vavelin <kvavelin@geek-inc.fr>
2020-06-24 22:58:34 +0200

    Start working on real app with real purpose

---
+++ Diary/Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by Kevin Vavelin on 24/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ Diary/Diary/RootView.swift
@@ -0,0 +1,20 @@
+//
+//  RootView.swift
+//  Diary
+//
+//  Created by Kevin Vavelin on 24/06/2020.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
